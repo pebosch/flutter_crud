71fa8223c0c4ea5081af439b49f4c52c5196adf6
Pedro Bosch <pedro@andared.com>
2026-01-22 09:31:58 +0100

    Lectura correcta

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_crud/services/firebase_service.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -18,14 +19,44 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({
+    super.key,
+  });
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Material App Bar'),
       ),
+      body: FutureBuilder(
+        future: getVideojuegos(), 
+        builder: ((context, snapshot) {
+          if(snapshot.hasData){
+            return ListView.builder(
+            itemCount: snapshot.data?.length,
+            itemBuilder: (context, index) {
+              return Text(snapshot.data?[index]['nombre']);
+            },
+          );
+          } else { return Center (
+              child: CircularProgressIndicator(),
+            );
+          }
+          
+        })
+      )
     );
   }
 }
